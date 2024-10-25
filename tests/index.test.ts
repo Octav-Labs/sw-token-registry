f9cef3566b8581ac7ee85bde697b919f2f944538
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-10-25 12:08:28 +0800

    add eslint prettier jest

---
+++ tests/index.test.ts
@@ -0,0 +1,9 @@
+// src/index.test.ts
+
+import { foo } from '../src/index';
+
+describe('Math functions', () => {
+  test('add should return the sum of two numbers', () => {
+    expect(foo).toBe('bar'); // Expected output: 5
+  });
+});
