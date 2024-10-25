ef147f17d8860bae13b4d76be3e1195ff24cae07
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-10-25 16:06:27 +0800

    test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,8 +2,8 @@
 
 import { foo } from '../src/index';
 
-describe('Math functions', () => {
-  test('add should return the sum of two numbers', () => {
-    expect(foo).toBe('bar'); // Expected output: 5
+describe('Constants', () => {
+  test('foo sould be bar', () => {
+    expect(foo).toBe('bar');
   });
 });
