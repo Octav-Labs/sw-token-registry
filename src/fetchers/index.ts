acbae549c72f893a3ab4f36a4fac00ba4d377965
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-11-29 10:45:53 +0800

    exports

--- src/fetchers/index.ts
+++ src/fetchers/index.ts
@@ -1,4 +1,7 @@
 import SolanaFetcher from './solana';
 import EvmFetcher from './evm';
+import BitcoinFetcher from './bitcoin';
+import AptosFetcher from './aptos';
+import SuiFetcher from './sui';
 
-export { SolanaFetcher, EvmFetcher };
+export { SolanaFetcher, EvmFetcher, BitcoinFetcher, AptosFetcher, SuiFetcher };
