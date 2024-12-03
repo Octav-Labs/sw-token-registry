52c1005345858e70f3d37b7a2ea920acb7976944
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-12-03 11:30:40 +0800

    fix

--- src/helpers/solanaTokens.ts
+++ src/helpers/solanaTokens.ts
@@ -4,24 +4,31 @@ import {
 } from '@sonarwatch/portfolio-core';
 import { Token } from '../types';
 
-export const solanaTokens: Omit<Token, 'chainId' | 'networkId'>[] = [
-  {
-    address: 'xAURp5XmAG7772mfkSy6vRAjGK9JofYjc3dmQDWdVDP',
-    decimals: 9,
-    name: 'Aurory',
-    symbol: 'xAURY',
-    logoURI:
-      'https://raw.githubusercontent.com/sonarwatch/token-registry/main/img/solana/xAURp5XmAG7772mfkSy6vRAjGK9JofYjc3dmQDWdVDP.webp',
-  },
-];
+export const solanaTokens: Omit<Token, 'chainId' | 'networkId' | 'logoURI'>[] =
+  [
+    {
+      address: 'xAURp5XmAG7772mfkSy6vRAjGK9JofYjc3dmQDWdVDP',
+      decimals: 9,
+      name: 'Aurory',
+      symbol: 'xAURY',
+    },
+    {
+      address: 'vPtS4ywrbEuufwPkBXsCYkeTBfpzCd6hF52p8kJGt9b',
+      decimals: 6,
+      name: 'Vault Points',
+      symbol: 'VPTS',
+    },
+  ];
 
 export const solanaTokensMap = new Map(
   solanaTokens.map((t): [string, Token] => [
     uniformSolanaTokenAddress(t.address),
     {
       ...t,
+      address: uniformSolanaTokenAddress(t.address),
       chainId: 101,
       networkId: NetworkId.solana,
+      logoURI: `https://raw.githubusercontent.com/sonarwatch/token-registry/main/img/solana/${t.address}.webp`,
     },
   ])
 );
