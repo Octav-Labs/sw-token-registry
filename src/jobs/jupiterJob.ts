fae644198672586be1db76e46ecf86109bb17d1d
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-12-03 11:12:26 +0800

    fix job

--- src/jobs/jupiterJob.ts
+++ src/jobs/jupiterJob.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { NetworkId } from '@sonarwatch/portfolio-core';
 import { Token, JobFct, Job } from '../types';
 import { solToken, wsolToken } from '../helpers/constants';
+import { solanaTokensMap } from '../helpers/solanaTokens';
 
 async function getJupTokens() {
   const res = await axios.get('https://tokens.jup.ag/tokens', {
@@ -19,6 +20,7 @@ const jobFct: JobFct = async () => {
     const jupToken = jupTokens[i];
     if (jupToken.address === solToken.address) continue;
     if (jupToken.address === wsolToken.address) continue;
+    if (solanaTokensMap.has(jupToken.address)) continue;
 
     tokens.set(jupToken.address, {
       address: jupToken.address,
