2ef1a7e0a23eceae79f3ccbac4a83514df73ea90
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-12-04 14:47:04 +0800

    tests

--- tests/integration/suiFetcher.test.ts
+++ tests/integration/suiFetcher.test.ts
@@ -10,12 +10,6 @@ describe('suiFetcher', () => {
     fetcher = new SuiFetcher(rpc);
   });
 
-  it('should return null if token address is not valid', async () => {
-    const address = 'address_that_doesnt_exist';
-    const tokenInfo = await fetcher.fetch(address);
-    expect(tokenInfo).toBe(null);
-  });
-
   it('should return null if address is valid but not a token', async () => {
     const tokenInfo = await fetcher.fetch(
       '0x5f3a18cdfd7ef0527a65ba5c07dbe0efe276507d4d1a4d1bebe87f5d40df6cf6::boby::BOBY'
@@ -32,6 +26,7 @@ describe('suiFetcher', () => {
     const address = '0x002::sui::SUI';
     const tokenInfo = await fetcher.fetch(address);
     expect(tokenInfo).not.toBeNull();
+    expect(tokenInfo?.logoURI).not.toBeUndefined();
     expect(tokenInfo?.symbol).toBe('SUI');
     expect(tokenInfo?.chainId).toBe(1);
     expect(tokenInfo?.decimals).toBe(9);
@@ -42,9 +37,21 @@ describe('suiFetcher', () => {
       '0x5f3a18cdfd7ef0527a65ba5c07dbe0efe276507d4d1a4d1bebe87f5d40df6cf6::bob::BOB';
     const tokenInfo = await fetcher.fetch(address);
     expect(tokenInfo).not.toBeNull();
+    expect(tokenInfo?.logoURI).not.toBeUndefined();
     expect(tokenInfo?.symbol).toBe('BOB');
     expect(tokenInfo?.chainId).toBe(1);
     expect(tokenInfo?.decimals).toBe(6);
+  });
+
+  it('should return AXOL token', async () => {
+    const address =
+      '0xae00e078a46616bf6e1e6fb673d18dcd2aa31319a07c9bc92f6063363f597b4e::AXOL::AXOL';
+    const tokenInfo = await fetcher.fetch(address);
+    expect(tokenInfo).not.toBeNull();
+    expect(tokenInfo?.logoURI).not.toBeUndefined();
+    expect(tokenInfo?.symbol).toBe('AXOL');
+    expect(tokenInfo?.chainId).toBe(1);
+    expect(tokenInfo?.decimals).toBe(9);
   });
 
   it('should return HASUI token', async () => {
@@ -62,6 +69,7 @@ describe('suiFetcher', () => {
       '0x671b1fa2a124f5be8bdae8b91ee711462c5d9e31bda232e70fd9607b523c88::scallop_af_sui::SCALLOP_AF_SUI'
     );
     expect(tokenInfo).not.toBeNull();
+    expect(tokenInfo?.logoURI).not.toBeUndefined();
     expect(tokenInfo?.symbol).toBe('sAfSUI');
     expect(tokenInfo?.chainId).toBe(1);
     expect(tokenInfo?.decimals).toBe(9);
@@ -70,6 +78,7 @@ describe('suiFetcher', () => {
       '671b1fa2a124f5be8bdae8b91ee711462c5d9e31bda232e70fd9607b523c88::scallop_af_sui::SCALLOP_AF_SUI'
     );
     expect(tokenInfo2).not.toBeNull();
+    expect(tokenInfo2?.logoURI).not.toBeUndefined();
     expect(tokenInfo2?.symbol).toBe('sAfSUI');
   });
 });
