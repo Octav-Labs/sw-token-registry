b1ca0f5189a2e349888f77f27187cc77515f2357
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-05-15 19:06:54 +0800

    test: add validation tests for token schema

---
+++ tests/tokenSchema.test.ts
@@ -0,0 +1,35 @@
+// {
+//         address: 'EJPtJEDogxzDbvM8qvAsqYbLmPj5n1vQeqoAzj9Yfv3q',
+//         chainId: 101,
+//         decimals: 9,
+//         name: 'bozo',
+//         symbol: '',
+//         logoURI: 'https://nftstorage.link/ipfs/bafkreiamobqahwlwio5syavvfkknvfecgt7osbsh2s4xizihgpsajethyy',
+//         networkId: 'solana',
+//         sourceId: 'job-jupiter'
+//       }
+
+import tokenSchema from '../src/tokenSchema';
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+
+describe('Token Schema Validation', () => {
+  it('should validate a valid token object', async () => {
+    const ajv = new Ajv();
+    addFormats(ajv);
+    const ajvToken = ajv.compile(tokenSchema);
+    const token = {
+      address: 'EJPtJEDogxzDbvM8qvAsqYbLmPj5n1vQeqoAzj9Yfv3q',
+      chainId: 101,
+      decimals: 9,
+      name: 'bozo',
+      symbol: '',
+      logoURI:
+        'https://nftstorage.link/ipfs/bafkreiamobqahwlwio5syavvfkknvfecgt7osbsh2s4xizihgpsajethyy',
+      networkId: 'solana',
+      sourceId: 'job-jupiter',
+    };
+    const valid = ajvToken(token);
+    expect(valid).toBe(true);
+  });
+});
